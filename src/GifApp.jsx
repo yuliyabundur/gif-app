967053740e78559117a188bb7d16ed02f082c8f8
´Yuliya <ybundur@gmail.com>
2024-03-31 22:36:10 +0200

    adding first component

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,9 +1,40 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+
 
 export const GifApp = () => {
+
+    const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+    const onAddCategory = () => {
+        setCategories([ 'Valorant', ...categories ]);
+        // setCategories( cat => [...cat, 'Valorant'] ); - otra opción 
+        //console.log('Valorant');
+    };
+
+    //console.log (categories);
+
     return (
 
         <>
+
+            {/*titulo*/}
             <h1>GifApp</h1>
+
+            {/*Input*/}
+            <AddCategory/>
+
+            {/*Listado de Gif*/}
+            <button onClick={ onAddCategory }>Agregar</button>
+            <ol>
+                { 
+                categories.map( category => {
+                    return <li key={ category }> { category } </li>
+                })
+                }
+                
+            </ol>
+                {/*Gif item*/}
         </>
 
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { GifApp } from './GifApp.jsx';
+
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
